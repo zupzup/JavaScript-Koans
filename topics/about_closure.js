552201f1a164ebcc2a7b5f87c1a3e731935c0833
Stefan Mayer <stefanmayer13@gmail.com>
2014-12-07 21:51:27 +0100

    Added koans for closure

---
+++ topics/about_closure.js
@@ -0,0 +1,98 @@
+module("About Functions And Closure (topics/about_functions_and_closure.js)");
+
+test("simple closure", function() {
+    var result = "a";
+    function changeResult() {
+        result = "b";
+    };
+    changeResult();
+    ok(__ === result, 'what is the value of result?');
+});
+
+test("module pattern", function() {
+    var foo = (function CoolModule() {
+        var something = "cool";
+        var another = [1, 2, 3];
+
+        function doSomething() {
+            return something;
+        }
+
+        function doAnother() {
+            return another.join( " ! " );
+        }
+
+        return {
+            doSomething: doSomething,
+            doAnother: doAnother
+        };
+    })();
+
+    ok(__ === defined(something), 'is something available in this scope?');
+    ok(__ === defined(another), 'is something available in this scope?');
+    ok(__ === foo.doSomething(), 'what is the value returned by doSomething?');
+    ok(__ === foo.doAnother(), 'what is the value returned by doAnother?');
+});
+
+asyncTest("closure at for loop", function(assert) {
+    var sum = 0;
+    for (var i=1; i<=5; i++) {
+        setTimeout( function timer(){
+            sum += i;
+        }, i*5 );
+    }
+
+    setTimeout( function timer(){
+        ok(__ === sum, 'what is the value of sum?');
+        start();
+    }, 26 );
+});
+
+asyncTest("closure at for loop with explicit assignment", function(assert) {
+    var sum = 0;
+    for (var i=1; i<=5; i++) {
+        var j = i;
+        setTimeout( function timer(){
+            sum += j;
+        }, i*5 );
+    }
+
+    setTimeout( function timer(){
+        ok(__ === sum, 'what is the value of sum?');
+        start();
+    }, 26 );
+});
+
+asyncTest("IIFE",function(){
+    var sum = 0;
+    for (var i=1; i<=5; i++) {
+        (function(){
+            var j = i;
+            setTimeout( function timer(){
+                sum += j;
+            }, j*5 );
+        })();
+    }
+
+    setTimeout( function timer(){
+        ok(__ === sum, 'what is the value of sum?');
+        start();
+    }, 26 );
+});
+
+test("passing values on IIFE",function(){
+    var sum = 0;
+    for (var i=1; i<=5; i++) {
+        (function(j){
+            setTimeout( function timer(){
+                sum += j;
+            }, j*5 );
+        })( i );
+    }
+
+    setTimeout( function timer(){
+        ok(__ === sum, 'what is the value of sum?');
+        start();
+    }, 26 );
+});
+
